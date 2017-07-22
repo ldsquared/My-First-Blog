f9de751b7d39f7c98e4d66ed2c9d54bc537541d6
ldsquared <ldsquared11@gmail.com>
2017-07-22 14:45:48 -0500

    resetting

--- djangostartersite/settings.py
+++ djangostartersite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^dkk+2oi*@am418t-y-eck-t)&m88-=to*1l%s9qp_#**!*d*%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '<ldsquared11>.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CST'
 
 USE_I18N = True
 
@@ -119,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
